4ea04e5f4941ad56ee8d215833de15f5afc064ab
dkhovyakov@griddynamics.com <dkhovyakov@griddynamics.com>
2013-11-14 16:26:16 +0200

    Building now can be placed only near existing buildings

--- start_game.py
+++ start_game.py
@@ -27,12 +27,55 @@ button_homes = Button(0, 0, 115, 23)
 button_fields = Button(0, 0, 93, 23)
 building = None
 
+population = 100
+population_growth = 10
+food = 100
+turn = 0
+
+label_font = pygame.font.SysFont('monospace', 17)
+
 
 def setup_buttons(buttons_pos):
     button_homes.x = buttons_pos[0]
     button_homes.y = buttons_pos[1]
     button_fields.x = buttons_pos[0]
     button_fields.y = buttons_pos[1] + 23
+
+
+def build(mouse_x, mouse_y):
+    x = mouse_x / 32
+    y = mouse_y / 32
+
+    may_build = False
+    if x > 0:
+        if game_map[x - 1][y] != 'grass':
+            may_build = True
+        if y > 0 and game_map[x - 1][y - 1] != 'grass':
+            may_build = True
+        if y < 9 and game_map[x - 1][y - 1] != 'grass':
+            may_build = True
+    if x < 9:
+        if game_map[x + 1][y] != 'grass':
+            may_build = True
+        if y > 0 and game_map[x + 1][y - 1] != 'grass':
+            may_build = True
+        if y < 9 and game_map[x + 1][y + 1] != 'grass':
+            may_build = True
+    if y > 0 and game_map[x][y - 1] != 'grass':
+        may_build = True
+    if y < 9 and game_map[x][y + 1] != 'grass':
+        may_build = True
+    if game_map[x][y] != 'grass':
+        may_build = False
+    if may_build:
+        game_map[x][y] = building
+
+
+def next_turn():
+    global turn, population
+    turn += 1
+    population += population_growth
+
 
 while not done:
     for event in pygame.event.get():
@@ -52,10 +95,12 @@ while not done:
                     if button_fields.is_pressed(event.pos[0], event.pos[1]):
                         building = 'field'
                 elif building:
-                    pos = event.pos
-                    x = pos[0]/32
-                    y = pos[1]/32
-                    game_map[x][y] = building
+                    build(*event.pos)
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RETURN:
+                next_turn()
+
+    screen.fill((0, 0, 0))
 
     for x in range(0, 10):
         for y in range(0, 10):
@@ -69,5 +114,8 @@ while not done:
     if buttons_active:
         screen.blit(sprites['build_homes'], buttons_pos)
         screen.blit(sprites['build_field'], (buttons_pos[0], buttons_pos[1] + 23))
+
+    population_label = label_font.render("Population: %d" % population, 1, (255, 255, 255))
+    screen.blit(population_label, (320, 0))
 
     pygame.display.flip()
