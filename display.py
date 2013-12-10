b8399a919e068a0b2a0fdde358ea4ce51bb7855e
TriD <hovyakov@gmail.com>
2013-12-11 00:04:45 +0200

    Major refactoring. Moved drawing to outer class. Outer class is real shit.

---
+++ display.py
@@ -0,0 +1,58 @@
+import pygame
+from pygame.rect import Rect
+
+__author__ = 'TriD'
+
+
+class Display():
+    def __init__(self, game_data, uis):
+        pygame.init()
+        self.screen = pygame.display.set_mode([800, 600])
+        self.label_font = pygame.font.SysFont('monospace', 17)
+        self.game_data = game_data
+        self.uis = uis
+
+    def draw_sprite(self, x, y, sprite):
+        if x + 32 <= 600:
+            self.screen.blit(sprite, [x, y])
+        elif 632 > x + 32 > 600:
+            self.screen.blit(sprite, [x, y], Rect(0, 0, 600 - x, sprite.get_height()))
+
+    def draw(self):
+        self.screen.fill((0, 0, 0))
+
+        for x in range(0, 20):
+            for y in range(0, 20):
+                # New, better rendering that draws part of map that is currently on screen
+                mx = x + self.game_data.dx / 32
+                my = y + self.game_data.dy / 32
+                sx = x * 32 - self.game_data.dx % 32
+                sy = y * 32 - self.game_data.dy % 32
+                self.draw_sprite(sx, sy, self.uis.sprites[self.game_data.game_map[mx][my].ground])
+                if self.game_data.game_map[mx][my].resource:
+                    self.draw_sprite(sx, sy, self.uis.sprites[self.game_data.game_map[mx][my].resource])
+                if self.game_data.game_map[mx][my].building == 'center':
+                    self.draw_sprite(sx, sy - 64, self.uis.sprites['center'])
+                elif self.game_data.game_map[mx][my].building:
+                    self.draw_sprite(sx, sy, self.uis.sprites[self.game_data.game_map[mx][my].building])
+        for expedition in self.game_data.expeditions:
+            self.draw_sprite(expedition.x * 32 - self.game_data.dx, expedition.y * 32 - 28 - self.game_data.dy, self.uis.sprites['human'])
+
+        self.draw_sprite(self.game_data.monster.x * 32 - self.game_data.dx, self.game_data.monster.y * 32 - self.game_data.dy, self.uis.sprites['monster'])
+
+        if self.game_data.buttons_active:
+            self.uis.draw_buttons(self.screen)
+
+        population_label = self.label_font.render("Population: %d/%d" % (
+        self.game_data.village.population, self.game_data.village.max_population), 1,
+                                             (255, 255, 255))
+        self.screen.blit(population_label, (600, 0))
+        food_label = self.label_font.render("Food: %d(+%d)" % (
+        self.game_data.village.food_stockpile, self.game_data.village.food_growth), 1, (255, 255, 255))
+        self.screen.blit(food_label, (600, 20))
+        wood_label = self.label_font.render("Wood: %d(+%d)" % (
+        self.game_data.village.wood_stockpile, self.game_data.village.wood_increasing), 1,
+                                       (255, 255, 255))
+        self.screen.blit(wood_label, (600, 40))
+
+        pygame.display.flip()
--- start_game.py
+++ start_game.py
@@ -1,5 +1,5 @@
-from pygame.rect import Rect
 from GameData import GameData
+from display import Display
 from events import EventProcessor
 __author__ = 'TriD'
 
@@ -9,25 +9,8 @@ from ui_state import UIState
 game_data = GameData()
 uis = UIState(game_data)
 game_data.uis = uis
-
+display = Display(game_data, uis)
 event_processor = EventProcessor(game_data, uis)
-
-pygame.init()
-screen = pygame.display.set_mode([800, 600])
-
-done = False
-buttons_active = False
-buttons_pos = None
-
-turn = 0
-
-label_font = pygame.font.SysFont('monospace', 17)
-
-old_dx = 0
-old_dy = 0
-dx = 0
-dy = 0
-drag = False
 
 
 def build_houses():
@@ -51,47 +34,6 @@ uis.button_woodcutter.callback = build_woodcutter
 uis.button_expedition.callback = send_expedition_callback
 
 
-def draw(x, y, sprite):
-    global dx
-    if x + 32 <= 600:
-        screen.blit(sprite, [x, y])
-    elif 632 > x + 32 > 600:
-        screen.blit(sprite, [x, y], Rect(0, 0, 600 - x, sprite.get_height()))
-
-
 while not game_data.done:
     event_processor.process_events()
-    screen.fill((0, 0, 0))
-
-    for x in range(0, 20):
-        for y in range(0, 20):
-            # New, better rendering that draws part of map that is currently on screen
-            mx = x + game_data.dx / 32
-            my = y + game_data.dy / 32
-            sx = x * 32 - game_data.dx % 32
-            sy = y * 32 - game_data.dy % 32
-            draw(sx, sy, uis.sprites[game_data.game_map[mx][my].ground])
-            if game_data.game_map[mx][my].resource:
-                draw(sx, sy, uis.sprites[game_data.game_map[mx][my].resource])
-            if game_data.game_map[mx][my].building == 'center':
-                draw(sx, sy - 64, uis.sprites['center'])
-            elif game_data.game_map[mx][my].building:
-                draw(sx, sy, uis.sprites[game_data.game_map[mx][my].building])
-    for expedition in game_data.expeditions:
-        draw(expedition.x * 32 - dx, expedition.y * 32 - 28 - dy, uis.sprites['human'])
-
-    draw(game_data.monster.x * 32 - game_data.dx, game_data.monster.y * 32 - game_data.dy, uis.sprites['monster'])
-
-    if game_data.buttons_active:
-        uis.draw_buttons(screen)
-
-    population_label = label_font.render("Population: %d/%d" % (game_data.village.population, game_data.village.max_population), 1,
-                                         (255, 255, 255))
-    screen.blit(population_label, (600, 0))
-    food_label = label_font.render("Food: %d(+%d)" % (game_data.village.food_stockpile, game_data.village.food_growth), 1, (255, 255, 255))
-    screen.blit(food_label, (600, 20))
-    wood_label = label_font.render("Wood: %d(+%d)" % (game_data.village.wood_stockpile, game_data.village.wood_increasing), 1,
-                                   (255, 255, 255))
-    screen.blit(wood_label, (600, 40))
-
-    pygame.display.flip()
+    display.draw()
